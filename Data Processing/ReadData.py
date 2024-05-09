958b64ed32d4ba3b1994504ce1312d8820c2c07a
LloydDash <653078479@qq.com>
2024-05-09 16:09:17 +0800

    change tokens into sentence

--- Data Processing/ReadData.py
+++ Data Processing/ReadData.py
@@ -1,33 +1,37 @@
-import torch
-from PIL import Image
 import os
+import torch
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
 
-from torch.utils.data import Dataset, DataLoader
 
 class CustomDataset(Dataset):
-    def __init__(self, filename):
-        self.filename = filename
-        self.image = []
-        self.token = []
-        self.label = []
-
-        f = open(os.path.join(filename, 'twitter2015/train.txt'), encoding='utf-8')
+    def load_data(self, data_path, file_name):
+        f = open(os.path.join(data_path, file_name), encoding='utf-8')
         data = []
-        imgs = []
-        auxlabels = []
+        data_sentence = []
+        data_label = []
+        data_img = []
+        data_auxlabel = []
+
         sentence = []
-        label= []
+        label = []
         auxlabel = []
         imgid = ''
         for line in f:
             if line.startswith('IMGID:'):
-                imgid = line.strip().split('IMGID:')[1]+'.jpg'
+                imgid = line.strip().split('IMGID:')[1] + '.jpg'
                 continue
-            if line[0]=="\n":
+            if line[0] == "\n":
                 if len(sentence) > 0:
-                    data.append((sentence,label))
-                    imgs.append('twitter2015_images/'+str(imgid))
-                    auxlabels.append(auxlabel)
+                    single_sentence = ' '.join(sentence[4:len(sentence)-1])
+                    data_sentence.append(single_sentence)
+                    label = label[4:len(label)-1]
+                    data_label.append(label)
+                    data.append((single_sentence,label))
+                    prefix = file_name.split('/')[0]+'_images'
+                    data_img.append(f'{prefix}/' + str(imgid))
+                    data_auxlabel.append(auxlabel)
                     sentence = []
                     label = []
                     imgid = ''
@@ -43,70 +47,74 @@ class CustomDataset(Dataset):
             label.append(cur_label)
             auxlabel.append(cur_label[0])
 
-        if len(sentence) >0:
-            data.append((sentence,label))
-            imgs.append(imgid)
-            auxlabels.append(auxlabel)
-            sentence = []
-            label = []
-            auxlabel = []
-
+        if len(sentence) > 0:
+            single_sentence = ' '.join(sentence[4:len(sentence)-1])
+            data_sentence.append(single_sentence)
+            label = label[4:len(label)-1]
+            data_label.append(label)
+            data.append((single_sentence,label))
+            prefix = file_name.split('/')[0]+'_images'
+            data_img.append(f'{prefix}/' + str(imgid))
+            data_auxlabel.append(auxlabel)
+  
         f.close()
-        f = open(os.path.join(filename, 'twitter2017/train.txt'), encoding='utf-8')
-        imgid = ''
-        for line in f:
-            if line.startswith('IMGID:'):
-                imgid = line.strip().split('IMGID:')[1]+'.jpg'
-                continue
-            if line[0]=="\n":
-                if len(sentence) > 0:
-                    data.append((sentence,label))
-                    imgs.append('twitter2017_images/'+str(imgid))
-                    auxlabels.append(auxlabel)
-                    sentence = []
-                    label = []
-                    imgid = ''
-                    auxlabel = []
-                continue
-            splits = line.split('\t')
-            sentence.append(splits[0])
-            cur_label = splits[-1][:-1]
-            if cur_label == 'B-OTHER':
-                cur_label = 'B-MISC'
-            elif cur_label == 'I-OTHER':
-                cur_label = 'I-MISC'
-            label.append(cur_label)
-            auxlabel.append(cur_label[0])
+        return data, data_img 
 
-        if len(sentence) >0:
-            data.append((sentence,label))
-            imgs.append(imgid)
-            auxlabels.append(auxlabel)
-            sentence = []
-            label = []
-            auxlabel = []
+    def __init__(self, filename, image_preprocess=None, text_preprocess=None, max_len=32):
+        self.filename = filename
+        self.image = []
+        self.sentence = []
+        self.label = []
+        self.image_preprocess = image_preprocess
+        self.text_preprocess = text_preprocess
+        self.max_len = max_len
+        # 读取数据
+        data, imgs = self.load_data(filename, 'twitter2015/train.txt')
+        data_1, imgs_1 = self.load_data(filename, 'twitter2017/train.txt')
+        # 合并数据
+        data.extend(data_1)
+        imgs.extend(imgs_1)
 
         for i in range(len(data)):
-            for j in range(len(data[i][0])):
-                if(j>3 and j< len(data[i][0])-2):
-                    self.token.append(data[i][0][j])
-                    self.label.append(data[i][1][j])
-                    self.image.append(imgs[i])
+            self.sentence.append(data[i][0])
+            self.label.append(data[i][1])
+            self.image.append(imgs[i])
+        # image和token类型转换
+        self.image = np.array(self.image)
 
+        setlabel = []
+        for i in self.label:
+            for j in i:
+                setlabel.append(j)
+        setlabel = set(setlabel)
+
+        dictlabel = {'B-ORG':[0,0,0,0,0,0,0,0,1], 'B-MISC':[0,0,0,0,0,0,0,1,0], 'I-ORG':[0,0,0,0,0,0,1,0,0], 
+                     'B-PER':[0,0,0,0,0,1,0,0,0], 'I-MISC':[0,0,0,0,1,0,0,0,0], 'O':[0,0,0,1,0,0,0,0,0], 
+                     'I-PER':[0,0,1,0,0,0,0,0,0], 'I-LOC':[0,1,0,0,0,0,0,0,0], 'B-LOC':[1,0,0,0,0,0,0,0,0]}
+        for i in range(len(self.label)):
+            for j in range(len(self.label[i])):
+                self.label[i][j] = torch.tensor(dictlabel[self.label[i][j]])
+        
     def __len__(self):
         return len(self.token)
 
     def __getitem__(self, index):
         # 获取数据和标签
-        image = os.path.join(self.filename, self.image[index])
-        token = self.token[index]
+        image = Image.open(os.path.join(self.filename, self.image[index]))
+        if self.image_preprocess:
+            image = self.image_preprocess(image)
+        sentence = self.sentence[index]
+
+        if self.text_preprocess:
+            sentence = self.text_preprocess(sentence, max_length=self.max_len, truncation=True, padding="max_length", return_tensors='pt')
+            for key, value in sentence.items():
+                sentence[key] = torch.squeeze(value, dim=0)
+
         label = self.label[index]
 
-        # 进行任何必要的处理，例如：数据增强、标准化等
-        # data = ...
+        return image, sentence, label
 
-        return image, token, label
 
 if __name__ == "__main__":
     A = CustomDataset('../IJCAI2019_data')
-    print(A[0],A[2])
+    print(A[0])
